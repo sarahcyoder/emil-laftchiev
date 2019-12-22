9b5cab7ebf449967015403b005d7fd75be7f0bda
Sarah Yoder <syoder@thirdandgrove.com>
2019-12-22 11:49:08 -0500

    Add style sheet for publication teasers.

--- src/components/publicationTeaser.js
+++ src/components/publicationTeaser.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import containerStyles from './publicationTeaser.module.css';
 
 const PublicationTeaser = ({ article }) => {
 
   return (
-    <div>
-      <Link to={article.path.alias}>
+    <div className={containerStyles.card}>
+      <Link className={containerStyles.link} to={article.path.alias}>
         <h3>{article.title}</h3>
+        <div className={containerStyles.body} dangerouslySetInnerHTML={{ __html: article.body.processed }}></div>
+        <span>Read More</span>
       </Link>
     </div>
   );
