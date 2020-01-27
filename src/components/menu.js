ded4bdd1814f38941cf4037c6ae568685cbca9f8
Sarah Yoder <syoder@thirdandgrove.com>
2020-01-27 08:15:58 -0500

    Link to publications page in menu.

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
   return (
     <div className={containerStyles.container}>
       <Link to='/'>Home</Link>
-      <Link to='/'>Publications</Link>
+      <Link to='/publications'>Publications</Link>
       <Link to='/'>Teaching</Link>
       <Link to='/contact'>Contact</Link>
     </div>
