799c2b1cf61a58f1de09750053602c246d868fe6
Sarah Yoder <syoder@thirdandgrove.com>
2020-01-30 20:38:06 -0500

    Add updated copy to homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,23 @@ export default ({ data }) => {
         }}  
       >
       <SEO title="Home" />
-      <h2>Principal Research Scientist</h2>
-      <p>Emil currently works in the Data Analytics group at Mitsubishi Electric Research Laboratories. Emil's research focuses on machine learning, data mining techniques in anomaly detection, time series analytics, and predictive modeling.</p>
-      <p>Here are some of his most recent publications.</p>
+      <h2>Emil Laftchiev, Principal Research Scientist</h2>
+      <p>I am a machine learning scientist at MERL. I work with time series data,
+        envisioning and creating machine learning and computer science algorithms.
+        These algorithms find applications in emerging technologies like the Internet
+        of Things and wearable devices, and focus on learning user preferences, anomaly
+        detection, and time series prediction. My current focus is on deep learning
+        methods of generative modeling such as VAEs and GANs as well as modern methods
+        of time series analysis. Ongoing themes of my research include time series
+        pattern matching, anomaly detection, machine learning for big data, and
+        predictive modeling.</p>
+      <p>At MERL I am involved in directing the research direction by proposing and
+        executing new projects, maintaining state-of-the-art knowledge in machine
+        learning, publishing new algorithms and findings in competitive academic
+        venues, patenting applications relevant to the business, developing prototypes
+        for demonstration to senior management, and transferring knowledge to my
+        international counterparts in Japan.</p>
+      <p>Here are some of my most recent publications.</p>
       <PublicationsList id="portfolio" data={publications}></PublicationsList>
       <div id="button"><Link to='/publications'>View All Publications</Link></div>
     </Layout>
